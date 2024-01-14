4e9cd031152d68ab203deb633db24bdce77fe986
Ryan <rjeziorski@outlook.com>
2024-01-14 12:10:09 -0600

    Init, finished tutorial

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::env;           // gives us access to enviornment variables
+use std::process;       // gives us access to process functions, namely exit/abort
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+    
+    // dbg!(args);
+}
